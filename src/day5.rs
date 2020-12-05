9df788c379db158d34db468394c92e243b53d4ba
barsa2000 <barsanti-filippo2@hotmail.it>
2020-12-05 18:35:16 +0100

    refractored day5

--- src/day5.rs
+++ src/day5.rs
@@ -1,27 +1,59 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use std::error::Error;
 
+
 #[aoc_generator(day5)]
-fn parse_input(input: &str) -> Result<Vec<u16>, Box<dyn Error>> {
+fn parse_input_bin(input: &str) -> Result<Vec<u16>, Box<dyn Error>> {
+    input
+        .lines()
+        .map(|l| {
+            Ok(l.chars().fold(0_u16, |n, c| {
+                n << 1
+                    | match c {
+                        'R' | 'B' => 1,
+                        _ => 0,
+                    }
+            }))
+        })
+        .collect()
+}
+
+#[aoc(day5, part1)]
+fn part1_bin(passes: &Vec<u16>) -> u16 {
+    *passes.iter().max().unwrap()
+}
+
+#[aoc(day5, part2)]
+fn part2_bin(passes: &Vec<u16>) -> u16 {
+    // *passes.iter().max().unwrap()
+    let mut ids = passes.clone();
+    ids.sort();
+    ids.windows(2).find(|x| x[1] != x[0] + 1).unwrap()[0] + 1
+}
+
+
+#[aoc_generator(day5, part2, orig)]
+#[aoc_generator(day5, part1, orig)]
+fn parse_input_orig(input: &str) -> Result<Vec<u16>, Box<dyn Error>> {
     input
         .lines()
         .map(|l| Ok(calc_row(l) * 8 + calc_col(l)))
         .collect()
 }
 
-#[aoc(day5, part1)]
-fn part1(passes: &Vec<u16>) -> u16 {
+#[aoc(day5, part1, orig)]
+fn part1_orig(passes: &Vec<u16>) -> u16 {
     *passes.iter().max().unwrap()
 }
 
-#[aoc(day5, part2)]
-fn part2(passes: &Vec<u16>) -> u16 {
+#[aoc(day5, part2, orig)]
+fn part2_orig(passes: &Vec<u16>) -> u16 {
     let mut ids = passes.clone();
     ids.sort();
     ids.iter()
         .zip(ids.iter().skip(1))
         .find(|(i, n)| **n != **i + 1)
-        .unwrap()
+        .unwrap_or((&0, &0))
         .0
         + 1
 }
@@ -48,6 +80,7 @@ fn calc_col(s: &str) -> u16 {
 
 fn binary_space_part(code: &str, start: u16, len: u16, upper_c: char, lower_c: char) -> u16 {
     if code.len() == 0 {
+        // println!("{:#016b}", start);
         return start;
     }
 
@@ -79,8 +112,15 @@ mod tests {
     fn sample1() {
         let input = "FBFBBFFRLR";
 
+        println!(
+            "{:#016b}\n{:#016b}",
+            parse_input(input).unwrap().iter().next().unwrap(),
+            parse_input_bin(input).unwrap().iter().next().unwrap()
+        );
+
         // println!("{:?}", part2(input));
-        // assert_eq!(part2(input), 1);
-        assert_eq!(part1(&parse_input(input).unwrap()), 357);
+        assert_eq!(parse_input(input).unwrap(), parse_input_bin(input).unwrap());
+        assert!(false);
+        // assert_eq!(part2(&parse_input(input).unwrap()), 357);
     }
 }
