ba62155fe7a24d93998a037d3efbbb2786327227
barsa2000 <barsanti-filippo2@hotmail.it>
2020-12-15 13:50:07 +0100

    formatted day15

--- src/day15.rs
+++ src/day15.rs
@@ -1,5 +1,4 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use regex::Regex;
 use std::collections::HashMap;
 
 #[aoc_generator(day15)]
@@ -9,34 +8,40 @@ fn parse_input(input: &str) -> Vec<u64> {
 
 #[aoc(day15, part1)]
 fn part1(starting_numbers: &[u64]) -> u64 {
-    solve(starting_numbers,2020 )
+    solve(starting_numbers, 2020)
 }
 
 #[aoc(day15, part2)]
 fn part2(starting_numbers: &[u64]) -> u64 {
-    solve(starting_numbers,30000000 )
+    solve(starting_numbers, 30000000)
 }
 
 fn solve(starting_numbers: &[u64], max: u64) -> u64 {
-    let mut numbers:HashMap<u64,u64> = HashMap::new();
+    let mut numbers: HashMap<u64, u64> = HashMap::new();
 
-    starting_numbers.iter().take(starting_numbers.len() - 1).enumerate().for_each(|(i,n)| {numbers.insert(*n, i as u64);});
+    starting_numbers
+        .iter()
+        .take(starting_numbers.len() - 1)
+        .enumerate()
+        .for_each(|(i, n)| {
+            numbers.insert(*n, i as u64);
+        });
 
     let mut turn = starting_numbers.len() as u64;
 
-    let mut last_spoken:u64 = *starting_numbers.iter().last().unwrap();
+    let mut last_spoken: u64 = *starting_numbers.iter().last().unwrap();
 
     while turn < max {
         let curr;
 
-        if let Some(a) =  numbers.get(&last_spoken) {
+        if let Some(a) = numbers.get(&last_spoken) {
             curr = turn - a - 1;
         } else {
             curr = 0;
         }
         numbers.insert(last_spoken, turn - 1);
         last_spoken = curr;
-        turn +=1;
+        turn += 1;
     }
 
     last_spoken
