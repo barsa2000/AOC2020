0d1bf496354635be82a7c50ea3c31b5d7666fc81
barsa2000 <barsanti-filippo2@hotmail.it>
2020-12-18 22:47:05 +0100

    formatted files

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ use aoc_runner_derive::aoc_lib;
 
 #[macro_use]
 extern crate lazy_static;
-extern crate mut_static;
-extern crate ndarray;
 extern crate binary_tree;
+extern crate mut_static;
 
 mod day01;
 mod day02;
